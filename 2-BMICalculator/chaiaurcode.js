fd1ba9739ec59155d87e347c99f181251d2e5095
Soham13anerjee <sohambanerg@gmail.com>
2024-01-13 23:22:26 +0530

    project 2 task 2

--- 2-BMICalculator/chaiaurcode.js
+++ 2-BMICalculator/chaiaurcode.js
@@ -19,6 +19,20 @@ form.addEventListener('submit',function(event){
     else if(weight === '' || weight<0 || isNaN(weight)){
         results.innerText = `invalid weight. re enter weight. ${weight}`;
     }else{
-        results.innerText = (weight/(height*height/10000)).toFixed(2);
+        if(form.lastChild.id==='output'){
+            form.removeChild(form.lastChild);
+        }
+        const bmi = (weight/(height*height/10000)).toFixed(2);
+        results.innerText = bmi;
+        const output = document.createElement('div');
+        if(bmi<18.6){
+            output.innerText = 'Underweight';
+        }else if(bmi>=18.6 && bmi<=24.9){
+            output.innerText = 'Normal';
+        }else{
+            output.innerText = 'Overweight';
+        }
+        output.id = 'output';
+        form.appendChild(output);
     }
 })
