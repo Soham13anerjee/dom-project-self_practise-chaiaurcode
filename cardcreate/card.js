faf70244c8591618826c7a39e3aa8bcb05d558e2
Soham13anerjee <sohambanerg@gmail.com>
2024-01-18 22:11:06 +0530

    card project from github api

---
+++ cardcreate/card.js
@@ -0,0 +1,27 @@
+// let data;
+// const apiLink = 'https://api.github.com/users/Soham13anerjee';
+// const xhr = new XMLHttpRequest();
+// console.log('hi');
+// xhr.open('GET',apiLink);
+// xhr.send();
+// xhr.onreadystatechange = funtion(){
+//     console.log("soham");
+//     // if(xhr.readyState === 4){
+//     //     data = JSON.parse(this.responseText);
+//         // document.querySelector('button').removeAttribute('hidden');
+//     //     console.log(data);
+//     // }
+// }
+console.log("hi");
+const requestUrl = "https://api.github.com/users/Soham13anerjee";
+// const xhr = new XMLHttpRequest();
+// xhr.open("GET", requestUrl);
+// xhr.onreadystatechange = function () {
+//   console.log(xhr.readyState);
+//   if (xhr.readyState === 4) {
+//     const data = JSON.parse(this.responseText);
+//     console.log(typeof data);
+//     console.log(data.followers);
+//   }
+// };
+// xhr.send();
